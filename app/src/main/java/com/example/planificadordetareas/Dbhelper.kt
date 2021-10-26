17050c1d3a24441569bea3fa4ded5dadab248ba5
Eric jose navarro german <enavarro.extreme@gmail.com>
2021-10-25 21:27:02 -0500

    tabla y clase tareas
    tabla relacionada con usuarios

--- app/src/main/java/com/example/planificadordetareas/Dbhelper.kt
+++ app/src/main/java/com/example/planificadordetareas/Dbhelper.kt
@@ -4,14 +4,14 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-
 class Dbhelper(contex: Context?,name:String, cursor: SQLiteDatabase.CursorFactory?, version:Int): SQLiteOpenHelper(
     contex, name, cursor, version) {
-
 
     override fun onCreate(db: SQLiteDatabase?) {
 
         db?.execSQL("CREATE TABLE IF NOT EXISTS Usuarios(id INTEGER PRIMARY KEY AUTOINCREMENT, usuario TEXT UNIQUE, contraseña TEXT, preguntaSeguridad TEXT)")
+
+        db?.execSQL("CREATE TABLE IF NOT EXISTS Listatareas(id INTEGER PRIMARY KEY AUTOINCREMENT, dia TEXT, nombre TEXT, descripcion TEXT, idusuario INTEGER, FOREIGN KEY (idusuario) REFERENCES Usuarios(id))")
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
---
+++ app/src/main/java/com/example/planificadordetareas/Tareas.kt
@@ -0,0 +1,7 @@
+package com.example.planificadordetareas
+
+class Tareas (
+    var id:Int,
+    var tarea:String,
+    var descripcionTareas:String
+        )
